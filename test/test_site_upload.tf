c8e3712764b0e76277c72f54e844552e639c436c
J03ll <udujoel@gmail.com>
2021-04-08 03:48:23 +0100

    add storage accout, test site, variables

---
+++ test/test_site_upload.tf
@@ -0,0 +1,9 @@
+#Add the sample index.html to blob storage
+resource "azurerm_storage_blob" "example" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source                 = "index.html"
+}

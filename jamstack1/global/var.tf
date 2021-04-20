d94f74f25130d034800f38e2ab6b292a2b9605c2
J03ll <udujoel@gmail.com>
2021-04-20 11:11:19 +0100

    reorganise file layout

--- jamstack1/global/var.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group" {
-    type = string
-    description = "Name of Resource Group"
-}
-
-variable "location" {
-    type = string
-    description = "Location for Resources"
-}
